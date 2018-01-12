964953cffd8ee939d660ac5803ebc70e93d24180
r0nshaw <ron.shaw@rocketmail.com>
2018-01-12 14:10:54 -0800

    problem-3 complete

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function multiply(a,b){ //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumOfThreeNums = a + b + c;
+  var productOfThreeNums = a * b * c;
+  var thirdElement = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThreeNums + '.';
+  var fourthElement = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOfThreeNums + '.';
+  return [sumOfThreeNums, productOfThreeNums, thirdElement, fourthElement];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
